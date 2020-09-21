e0f45adff5b528ec99dc55c0dacfdb4b81347d35
syhnz1102 <syhnz1102@gmail.com>
2020-09-21 18:11:24 +0900

    type별 샘플 만들기

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Main from '@/components/Main'
+import Type01 from '@/components/Type01'
+import Type02 from '@/components/Type02'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Main',
+      component: Main
+    },
+
+    {
+      path: '/Type01',
+      name: 'Type01',
+      component: Type01
+    },
+
+    {
+      path: '/Type02',
+      name: 'Type02',
+      component: Type02
     }
   ]
 })
