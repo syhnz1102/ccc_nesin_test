c533367de78e9aa3021d9e6a516242f6434a8f75
syhnz1102 <syhnz1102@gmail.com>
2020-09-28 10:03:25 +0900

    학생화면동작 80%ok 채팅부분 하는중

---
+++ src/components/Student/shared/Header.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="header">
+      <div class="logo">
+         <span class="img">
+            <img src="@/assets/student/images/img_logo_sample.png" />
+         </span>
+         <strong>화상 상담</strong>
+      </div>
+      <div class="button">
+         <button @click="handleConsultExitBtnClick">상담종료</button>
+      </div>
+   </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+     handleConsultExitBtnClick() { //안됨
+        window.close();
+     }
+  }
+}
+</script>
